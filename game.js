7b20d9815b602b02152756bb4f3df843587558d6
Greg Sexton <gregsexton@gmail.com>
2011-06-02 22:26:30 +0100

    Added score, uses font from Google.

--- game.js
+++ game.js
@@ -11,11 +11,15 @@ function Game(canvasWidth, canvasHeight, canvasID){
     this.playing  = false;
 
     this.increaseSpeed = 1;
+
+    this.score = -1;
+    this.scoreDelta = 1;
 }
 
 Game.prototype.update = function (){
     this.ribbon.update();
     this.terrain.update();
+    this.updateScore();
     this.draw();
     return this.detectCollision();
 }
@@ -23,6 +27,7 @@ Game.prototype.update = function (){
 Game.prototype.draw = function (){
     this.ribbon.draw();
     this.terrain.draw();
+    this.drawScore();
 }
 
 Game.prototype.detectCollision = function (){
@@ -41,12 +46,42 @@ Game.prototype.detectCollision = function (){
     return true;
 }
 
+Game.prototype.updateScore = function (){
+    this.score += this.scoreDelta;
+}
+
 Game.prototype.makeMoreDifficult = function (){
     //TODO: need to put caps on this!
     if(this.increaseSpeed == 0){
         this.ribbon.increaseWidth(1);
         this.terrain.increaseWidth(1);
+        this.scoreDelta += 2;
     }
     this.terrain.increaseGap(-2);
+    this.scoreDelta += 1;
     this.increaseSpeed = (this.increaseSpeed+1)%10; //increase speed on every tenth call
+}
+
+Game.prototype.drawScore = function (){
+    var canvas = document.getElementById(this.canvasID);
+    var ctx    = canvas.getContext("2d");
+
+    ctx.font          = "bold 24px 'Luckiest Guy', sans-serif";
+    ctx.textBaseline  = 'bottom';
+    ctx.shadowColor   = '#000000';
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 4;
+    ctx.fillStyle     = '#f8ed43';
+
+    var score = pad(this.score.toString(), 8);
+    ctx.fillText(score, 10, this.maxy-10);
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+}
+
+function pad(inStr, len){
+    var str = inStr;
+    while(str.length < len)
+        str = '0' + str;
+    return str;
 }
