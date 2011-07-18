29f7914332680d9e54af37bc7624282d85bb650e
Greg Sexton <gregsexton@gmail.com>
2011-07-18 22:57:48 +0100

    Scoreboard table using AJAX built from python script.

--- main.js
+++ main.js
@@ -40,13 +40,36 @@ window.onload = function init(){
     }
     reset();
 
-    $("#scoreboard-container").load("http://www.gregsexton.org/ribbon/score.py", 
-        function (text, status, request){
+    $.getJSON("score.py",
+        function (data, status, request){
             if(status != "success"){
                 $("#scoreboard-container").after("<p>Error loading scoreboard.</p>");
+                return;
             }
+
+            buildScoreboard(data);
         }
     );
+}
+
+function buildScoreboard(data){
+    //pre-condition: data should be a list of maps, each should have a 'name' and 'score' key.
+    var str = '<table class="scoreboard-table">';
+    for(var i=0; i<data.length; i++){
+        if(i == 0)
+            str += '<tr class="scoreboard-top-row">';
+        else if(i%2 == 0)
+            str += '<tr class="scoreboard-row">';
+        else
+            str += '<tr class="scoreboard-row-alternate">';
+        str += '<td>'+ (i+1).toString() +'</td>';
+        str += '<td>' + data[i]['name'] + '</td>';
+        str += '<td class="score">' + data[i]['score'] + '</td>';
+        str += '</tr>';
+    }
+
+    str += '</table>';
+    $("#scoreboard-container").after(str);
 }
 
 window.onkeydown = function (event){
